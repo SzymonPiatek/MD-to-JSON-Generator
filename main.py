e1277d56a6ae0d4260d7b1f8b72f90a2922a9816
Szymon Piątek <szymon.piatek125@gmail.com>
2023-11-24 23:35:13 +0100

    Delete Comments

--- main.py
+++ main.py
@@ -7,16 +7,10 @@ def extract_variables_from_markdown(file_path, output_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
 
-    # Konwertuj plik Markdown na HTML
     html_content = markdown.markdown(content)
-
-    # Zdefiniuj wzorzec dla zmiennych zapisanych w formie test_variable_name
     pattern = re.compile(r'\b([a-zA-Z]+_[a-zA-Z]+[a-zA-Z0-9_]*)\b')
-
-    # Znajdź wszystkie dopasowania wzorca w HTML
     matches = re.findall(pattern, html_content)
 
-    # Wypisz unikalne zmienne
     unique_variables = list(OrderedDict.fromkeys((matches)))
     json_data = {variable: "test" for variable in unique_variables}
     with open(output_path, 'w', encoding='utf-8') as json_file:
